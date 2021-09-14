793f28abb7ec778c72efbe0d57ffc1b565dd3f77
akundashkin <a.kundashkin@g.nsu.ru>
2021-09-14 16:49:06 +0700

    complete first task

--- task_1.py
+++ task_1.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 
 _ONE = [np.float32(1.0)]
@@ -23,6 +24,26 @@ def find_upl64(num):
     return [num[count - 1], count - 2]
 
 
+def find_max(num):
+    reslt = 0
+    cnt = 0
+    while not numpy.isinf(num[0]):
+        reslt = num[0]
+        num[0] = num[0] * 2
+        cnt += 1
+    return [reslt, cnt]
+
+
+def find_max32(num):
+    reslt = 0
+    cnt = 0
+    while not numpy.isinf(num[0]):
+        reslt = num[0]
+        num[0] = np.float32(num[0] * 2)
+        cnt += 1
+    return [reslt, cnt]
+
+
 def comparison(one, ones, e):
     ones.append(one)
     ones.append(one + e)
@@ -34,11 +55,17 @@ def comparison(one, ones, e):
 
 def main():
     res1 = find_upl32(_ONE)
-    print('Result_32', res1)
     res2 = find_upl64(__ONE)
-    print('Result_64', res2)
-    print('Compare_32', comparison(_ONE[0], _ONES, res1[0]))
-    print('Compare_64', comparison(__ONE[0], __ONES, res2[0]))
+    res_max_1 = find_max32(_ONE)
+    res_max_2 = find_max(__ONE)
+    # print('Result_32', res1)
+    # print('Result_64', res2)
+    # print('Compare_32', comparison(_ONE[0], _ONES, res1[0]))
+    # print('Compare_64', comparison(__ONE[0], __ONES, res2[0]))
+    print('Result_max32', res_max_1[0])
+    print('Result_max64', res_max_2[0])
+    print('Power_max32', res_max_1[1])
+    print('Power_max64', res_max_2[1])
 
 
 if __name__ == '__main__':
