d9baf26fdaabda4517bb09ede5f83e80ce44abc0
aleeexk <53943917+aleeexk@users.noreply.github.com>
2021-10-05 12:57:38 +0700

    beauty

---
+++ main.py
@@ -0,0 +1,20 @@
+from sem_tasks import task_1, task_2, task_3, task_4, task_5, task_6, task_7, task_8, task_9, task_10, task_11, task_12
+
+
+def main():
+    # task_1.main()
+    task_2.main()
+    task_3.main()
+    # task_4.main()
+    # task_5.main()
+    # task_6.main()
+    # task_7.main()
+    # task_8.main()
+    # task_9.main()
+    # task_10.main()
+    # task_11.main()
+    # task_12.main()
+
+
+if __name__ == '__main__':
+    main()
