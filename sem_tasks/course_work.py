0b67cb6939adc299ed2acc4650d3fb5fe7eb2e41
aleeexk <53943917+aleeexk@users.noreply.github.com>
2021-12-21 01:26:24 +0700

    new!

--- sem_tasks/course_work.py
+++ sem_tasks/course_work.py
@@ -4,60 +4,57 @@ from matplotlib import pyplot as plt
 from numpy.fft import fft, ifft, fftfreq
 
 lam = 2
-c = 100
-delta = 0
-L = 10.0
-x_1 = -L + delta
-x_N = L - delta
-t_1 = 0.0
-t_N = 1
+c = 2
 N = 1000
-h = (x_N - x_1) / N
+L = 10.0
+x0 = -L
+xN = L
+h = (xN - x0) / N
 Nt = 1000
-tau = (t_N - t_1) / Nt
-t = [t for t in np.arange(t_1, t_N, tau)]
-x = [x for x in np.arange(x_1, x_N, h)]
+t0 = 0
+tN = 100
+tau = (tN - t0) / Nt
+t = [t for t in np.arange(t0, tN, tau)]
+x = [x for x in np.arange(x0, xN, h)]
 
 
-def a_for_t0(x):
+def a_t0(x):
     return c * lam / (m.cosh(lam * x))
 
 
-a0 = [a_for_t0(x) for x in np.arange(x_1, x_N, h)]
-a = [a0]
-
-
-def a_for_tm(a, N):
-    a = np.array(a)
-    f = [0.0] * (N)
-    f = np.array(f)
-    f = a * np.exp(-2j * (abs(a)) ** 2 * tau)
-    f = fft(f, N, axis=-1)
-    f = f * np.exp(1j * 5 * tau * fftfreq(N, 1 / (m.pi * 100)) ** 2)
-    f = ifft(f, N, axis=-1)
+def a_for_tm(a0, f, Nx, tj):
+    f = a0 * np.exp(-2j * tau * (abs(f)) ** 2)
+    f = fft(f, Nx)
+    f = f * np.exp(-1j * tau * fftfreq(Nx, h) ** 2)
+    f = np.abs(ifft(f, Nx))
     return f
 
 
-p = 0
-while p < (len(t) - 1):
-    c = a_for_tm(a[p], N)
-    c = list(c)
-    a.append(c)
-    p += 1
+def init_a():
+    A = np.zeros((N, Nt))
+    for i in range(N):
+        A[i, 0] = a_t0(x[i])
+    return A
 
-A_2D = np.array(a)
-A_1D = A_2D.flatten()
-X, T = np.meshgrid(x, t)
-A = np.reshape(A_1D, T.shape)
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection='3d')
-ax.plot_surface(T, X, A)
-ax.set_xlabel('t')
-ax.set_ylabel('x')
-ax.set_zlabel('A')
-plt.show()
 
-a1 = np.conj(a)
-f = np.multiply(a, a1)
-f = f.transpose()
-z = sum(f)
+def creation():
+    b = init_a()
+    for tj in range(1, Nt):
+        psi = np.copy(b[:, tj - 1])
+        psi_fin = a_for_tm(b[:, 0], psi, N, tj)
+        b[:, tj] = psi_fin
+    return b
+
+
+if __name__ == '__main__':
+    u = creation()
+    u = np.transpose(u)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    X, T = np.meshgrid(x, t)
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(X, T, u, rstride=1, cstride=1, cmap='viridis')
+    ax.set_zlabel('A')
+    ax.set_xlabel('x')
+    ax.set_ylabel('t')
+    plt.show()
